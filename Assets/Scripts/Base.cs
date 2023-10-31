ad911935afc509ad03816f58d44d762343263cec
Charlie <charlie88531@gmail.com>
2023-11-01 01:16:29 +0800

    Add Item json

--- Assets/Scripts/Base.cs
+++ Assets/Scripts/Base.cs
@@ -1,17 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+using System.Text.Json;
+
+
+[System.Serializable]
+public class SerializableDictionary
+{
+    public Dictionary<int, int> ItemDic = new Dictionary<int, int>();
+}
 
 public class Base : MonoBehaviour
 {
     // Start is called before the first frame update
     public static Ability[] player = new Ability[4];
-    public static Dictionary<int,int> ItemDic = new Dictionary<int, int>(); //uid,quantity 
+    public static SerializableDictionary serializedItemDic = new SerializableDictionary();
+
 
     public static Vector3 playerVec;
     public static int enemyID;
     public static string sceneName;
-
+    public static string Bagdatapath = "Assets/Json/Bagjson";
     void Start()
     {
         DontDestroyOnLoad(this.gameObject);
@@ -31,11 +41,26 @@ public class Base : MonoBehaviour
 
     public static void SaveData()
     {
+        //Svae Bag info
+        string json = JsonUtility.ToJson(serializedItemDic); 
+        File.WriteAllText(Bagdatapath, json); 
+
 
     }
 
     public static void LoadData()
     {
-        
+        // Load Bag info
+        if (File.Exists(Bagdatapath))
+        {
+            string json = File.ReadAllText(Bagdatapath);
+            serializedItemDic = JsonUtility.FromJson<SerializableDictionary>(json);
+        }
+    }
+
+    
+    private void OnApplicationQuit()
+    {
+        SaveData();
     }
 }
